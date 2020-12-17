3ef90560f046ea17a1bdfe164872fc41a26016f2
Anuragtech02 <apal895@gmail.com>
2020-12-17 14:33:11 +0530

    Update

--- app/src/main/java/com/voicemail/Config.java
+++ app/src/main/java/com/voicemail/Config.java
@@ -2,5 +2,5 @@ package com.voicemail;
 
 public class Config {
     public static  String EMAIL ="anuragpal0226@gmail.com";  //gmail address
-    public static  String PASSWORD ="anuragpal4554"; //password
+    public static  String PASSWORD ="yourpassword"; //password
 }
